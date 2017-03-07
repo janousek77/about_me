b0365cf5a23912d3e9c076667fc493dcbce05e58
janousek77 <janousek77@gmail.com>
2017-03-07 14:06:29 -0800

    Created the new repository and added a README and gitignore

---
+++ README.md
@@ -0,0 +1,9 @@
+# About Me repository
+
+**Author** Christopher Janousek
+
+**Date** March 7, 20017
+
+## Description
+
+This is a repository for the about me lab project for Code fellows class 201
---
+++ app.js
@@ -0,0 +1,47 @@
+
+'use strict';
+
+var answer1 = prompt('We\'re going to play a guessing game about me. Are you ready? Yes or no answers only please');
+if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
+  alert('Sweet, let\'s do this.');
+} else if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
+  alert('Well we\'re going to do it anyway.');
+} else {
+  alert('Don'\'t care we\'re still doing it');
+}
+
+var answer2 = prompt('');
+if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
+  alert('');
+} else if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
+  alert('');
+} else {
+  alert('');
+}
+
+var answer3 = prompt('');
+if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
+  alert('');
+} else if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
+  alert('');
+} else {
+  alert('');
+}
+
+var answer4 = prompt('');
+if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
+  alert('');
+} else if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
+  alert('');
+} else {
+  alert('');
+}
+
+var answer5 = prompt('');
+if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
+  alert('');
+} else if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
+  alert('');
+} else {
+  alert('');
+}
