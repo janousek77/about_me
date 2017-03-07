034653c79991da87c16f3e0375bba89434844713
janousek77 <janousek77@gmail.com>
2017-03-07 14:50:43 -0800

    fixed errors that wouldn't populate responses added 6th question

--- app.js
+++ app.js
@@ -1,55 +1,60 @@
 'use strict';
 
-var answer1 = prompt('We\'re going to play a guessing game about me. Are you ready? Yes or no answers only please');
+var answer = prompt('We\'re going to play a guessing game about me. Are you ready? Yes or no answers only please');
 if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
   alert('Sweet, let\'s do this.');
 } else if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
   alert('Don\'t care we\'re going to do it anyway.');
 } else {
   alert('Remember yes or no answers only please. Here we go.');
-}
+};
+
 
 var answer2 = prompt('Do I have a dog named Pistol Pete?');
-if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
+if (answer2.toLowerCase() === 'yes' || answer2.toLowerCase() === 'y') {
   alert('You\'re god damn right I do');
-} else if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
+} else if (answer2.toLowerCase() === 'no' || answer2.toLowerCase() === 'n') {
   alert('You\'re wrong sorry.');
 } else {
   alert('Remember yes or no answers only please.');
-}
+};
+
 
 var answer3 = prompt('Do you believe that I almost made it to the olympics for soccer for a U17 group');
-if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
+if (answer3.toLowerCase() === 'yes' || answer3.toLowerCase() === 'y') {
   alert('You shouldn\'t have. I like soccer but I\'m nowhere near that good.');
-} else if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
+} else if (answer3.toLowerCase() === 'no' || answer.3toLowerCase() === 'n') {
   alert('Correct. I was shit compared to those guys.');
 } else {
   alert('Remember yes or no answers only please.');
-}
+};
+
 
 var answer4 = prompt('Do I play the guitar?');
-if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
+if (answer4.toLowerCase() === 'yes' || answer4.toLowerCase() === 'y') {
   alert('You are correct. I am terrible at it though.');
-} else if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
+} else if (answer4.toLowerCase() === 'no' || answer4.toLowerCase() === 'n') {
   alert('You\'re wrong sorry.');
 } else {
   alert('Remember yes or no answers only please.');
-}
+};
 
-var answer5 = prompt('');
-if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
-  alert('');
-} else if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
-  alert('You\'re wrong sorry.');
-} else {
-  alert('Remember yes or no answers only please.');
-}
 
-var answer2 = prompt('Do I have a dog named Pistol Pete?');
-if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
-  alert('You\'re god damn right I do');
-} else if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
+var answer5 = prompt('Would you believe I once stepped on a pile of rattlesnakes?');
+if (answer5.toLowerCase() === 'yes' || answer5.toLowerCase() === 'y') {
+  alert('You\'ve guessd correctly that shit was terrifying');
+} else if (answer5.toLowerCase() === 'no' || answer5.toLowerCase() === 'n') {
   alert('You\'re wrong sorry.');
 } else {
   alert('Remember yes or no answers only please.');
-}
+};
+
+
+var answer6 = prompt('Would you believe that I moved to Seattle because I love the weather?');
+if (answer6.toLowerCase() === 'yes' || answer6.toLowerCase() === 'y') {
+  alert('You\'re right. The sun is bullshit.');
+} else if (answer6.toLowerCase() === 'no' || answer6.toLowerCase() === 'n') {
+  alert('You\'re wrong sorry.');
+} else {
+  alert('Remember yes or no answers only please.');
+};
